f480c912049d32cab2e56eb92205cb46aa076032
nelsontky <nelson@ccb.wtf>
2023-12-12 06:11:59 +0800

    feat: add invalid splits test

--- packages/tiny-spl/tests/tiny-spl.ts
+++ packages/tiny-spl/tests/tiny-spl.ts
@@ -422,6 +422,131 @@ describe("tiny-spl", () => {
     expect(account.name).to.equal(metadataArgs.name);
   });
 
+  it("should not allow owner to split token to invalid amounts", async () => {
+    const assets = await CONNECTION.getAssetsByOwner({
+      ownerAddress: SIGNER.publicKey.toBase58(),
+      limit: 1,
+      sortBy: {
+        sortBy: "created",
+        sortDirection: "desc",
+      },
+    });
+
+    const newestAsset = assets.items[0];
+    const assetProof = await CONNECTION.getAssetProof(
+      new PublicKey(newestAsset.id)
+    );
+    const [cnftMetadata] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from(CNFT_METADATA_SEED),
+        new PublicKey(newestAsset.id).toBuffer(),
+      ],
+      PROGRAM.programId
+    );
+
+    const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
+      CONNECTION,
+      TREE_ID
+    );
+    const canopyDepth = treeAccount.getCanopyDepth();
+
+    // parse the list of proof addresses into a valid AccountMeta[]
+    const proofPath: AccountMeta[] = assetProof.proof
+      .map((node: string) => ({
+        pubkey: new PublicKey(node),
+        isSigner: false,
+        isWritable: false,
+      }))
+      .slice(0, assetProof.proof.length - (!!canopyDepth ? canopyDepth : 0));
+
+    const ixWithTooManySplits = await PROGRAM.methods
+      .split(
+        new PublicKey(newestAsset.id),
+        [...new PublicKey(assetProof.root.trim()).toBytes()],
+        new anchor.BN(newestAsset.compression.leaf_id),
+        newestAsset.compression.leaf_id,
+        [new anchor.BN(2), new anchor.BN(2)] // invalid split amounts
+      )
+      .accounts({
+        cnftMetadata,
+        leafOwner: SIGNER.publicKey,
+        leafDelegate: SIGNER.publicKey,
+        compressionProgram: COMPRESSION_PROGRAM_ID,
+        collectionMint: mint,
+        tinySplAuthority,
+        merkleTree: TREE_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority: SIGNER.publicKey,
+        bubblegumSigner,
+        collectionMetadata: metadata,
+        editionAccount: masterEdition,
+        logWrapper: SPL_NOOP_PROGRAM_ID,
+        mplBubblegumProgram: BUBBLEGUM_PROGRAM_ID,
+        newLeafOwner: SIGNER.publicKey,
+        tokenMetadataProgram: mplTokenMetadataProgramId,
+        treeAuthority,
+      })
+      .remainingAccounts(proofPath)
+      .instruction();
+
+    const modifyComputeUnits = ComputeBudgetProgram.setComputeUnitLimit({
+      units: 1_400_000,
+    });
+
+    const result1 = await sendAndConfirmIxs(
+      [modifyComputeUnits, ixWithTooManySplits],
+      SIGNER.publicKey,
+      [SIGNER],
+      true
+    );
+
+    console.log(result1.value.err);
+
+    const ixWithZero = await PROGRAM.methods
+      .split(
+        new PublicKey(newestAsset.id),
+        [...new PublicKey(assetProof.root.trim()).toBytes()],
+        new anchor.BN(newestAsset.compression.leaf_id),
+        newestAsset.compression.leaf_id,
+        [new anchor.BN(0), new anchor.BN(3)] // invalid split amounts
+      )
+      .accounts({
+        cnftMetadata,
+        leafOwner: SIGNER.publicKey,
+        leafDelegate: SIGNER.publicKey,
+        compressionProgram: COMPRESSION_PROGRAM_ID,
+        collectionMint: mint,
+        tinySplAuthority,
+        merkleTree: TREE_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        authority: SIGNER.publicKey,
+        bubblegumSigner,
+        collectionMetadata: metadata,
+        editionAccount: masterEdition,
+        logWrapper: SPL_NOOP_PROGRAM_ID,
+        mplBubblegumProgram: BUBBLEGUM_PROGRAM_ID,
+        newLeafOwner: SIGNER.publicKey,
+        tokenMetadataProgram: mplTokenMetadataProgramId,
+        treeAuthority,
+      })
+      .remainingAccounts(proofPath)
+      .instruction();
+
+    const result2 = await sendAndConfirmIxs(
+      [modifyComputeUnits, ixWithZero],
+      SIGNER.publicKey,
+      [SIGNER],
+      true
+    );
+
+    console.log(result2.value.err);
+
+    const errorCode1 = (result1.value?.err as any).InstructionError[1].Custom;
+    const errorCode2 = (result2.value?.err as any).InstructionError[1].Custom;
+    expect(errorCode1).to.equal(3001);
+    expect(errorCode2).to.equal(3001);
+  });
+
   it("should allow token owner to split token and allow metadata creator to close metadata account", async () => {
     const assets = await CONNECTION.getAssetsByOwner({
       ownerAddress: SIGNER.publicKey.toBase58(),
