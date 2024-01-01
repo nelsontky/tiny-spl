6bea04997245f617f0fd828dff5e846f9467d461
nelsontky <nelson@ccb.wtf>
2024-01-02 04:51:14 +0800

    feat: add faq

--- apps/frontend/src/app/features/homepage/components/MainPage.tsx
+++ apps/frontend/src/app/features/homepage/components/MainPage.tsx
@@ -4,7 +4,9 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Window, WindowContent, WindowHeader } from "react95";
+import { Anchor, Avatar, Window, WindowContent, WindowHeader } from "react95";
+
+import { Faq } from "./Faq";
 
 const AppWalletMultiButton = dynamic(
   () =>
@@ -30,15 +32,54 @@ export const MainPage = () => {
   );
 
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="py-12">
       <Window>
-        <WindowHeader>Welcome!</WindowHeader>
+        <WindowHeader className="flex gap-1 items-center">
+          <img width={24} height={24} src="/assets/smiley-face.png" alt="hi" />
+          <span>Welcome!</span>
+        </WindowHeader>
         <WindowContent>
-          Welcome to Tiny SPL! On this site you will be able to manage your tiny
-          SPL tokens. Click the button below to connect to your Solana wallet!
-          <div className="mt-4 w-full flex justify-center">
-            <AppWalletMultiButton>Get started!</AppWalletMultiButton>
+          <div className="flex gap-1 items-center mb-4">
+            <div className="hidden sm:block">
+              <Avatar
+                size={128}
+                square
+                src="/assets/rotated-tree.webp"
+                alt="tree"
+              />
+              <p className="italic">
+                A tree rotated to better resemble a{" "}
+                <Anchor
+                  href="https://docs.solana.com/learn/state-compression"
+                  target="_blank"
+                >
+                  merkle tree
+                </Anchor>
+                .
+              </p>
+            </div>
+            <img
+              className="min-w-0"
+              src="/assets/tinyspl-wordart.webp"
+              alt="word art"
+            />
           </div>
+          <div className="space-y-4">
+            <p>
+              Welcome! Tiny SPL is a new token standard on Solana that allows
+              you to own tokens and not pay for rent .
+            </p>
+            <p>
+              On this site you will be able to manage your tiny SPL tokens.
+              Click the button below to connect to your Solana wallet, or you
+              could read on to learn more before coming back to connect your
+              wallet!
+              <div className="mt-4 w-full flex justify-center">
+                <AppWalletMultiButton>Get started!</AppWalletMultiButton>
+              </div>
+            </p>
+          </div>
+          <Faq />
         </WindowContent>
       </Window>
     </div>
